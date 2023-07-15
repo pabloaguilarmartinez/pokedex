076750e8c2d4601a2c095c779ced0e923cafd2ba
aguilarpablo <pablooagm@gmail.com>
2023-07-15 09:28:16 +0200

    First commit

---
+++ src/main/java/com/heytrade/assessment/v2/pokedex/PokedexApplication.java
@@ -0,0 +1,13 @@
+package com.heytrade.assessment.v2.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexApplication.class, args);
+	}
+
+}
